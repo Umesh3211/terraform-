490b041423b8d57cd34141351a2962d3e2bd68f2
Umesh Kummar patro <umepatro@gmail.com>
2025-05-29 19:08:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+  region = "ap-south-1" # Replace with your desired region
+}
 
+resource "aws_instance" "example" {
+  ami           = "ami-0af9569868786b23a" # Replace with your desired AMI ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Test-Instance-1"
+  }
+
+}
